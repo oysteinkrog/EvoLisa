1b362400b9225850790bd257668d3d04c09377ed
Øystein Krog <oystein.krog@gmail.com>
2013-06-17 21:11:35 +0200

    Add MathUtils class with Clamp function

---
+++ Core/Classes/MathUtils.cs
@@ -0,0 +1,20 @@
+﻿namespace GenArt.Classes
+{
+    public static class MathUtils
+    {
+        public static int Clamp(int value, int min, int max)
+        {
+            return (value < min) ? min : (value > max) ? max : value;
+        }
+
+        public static float Clamp(float value, float min, float max)
+        {
+            return (value < min) ? min : (value > max) ? max : value;
+        }
+
+        public static double Clamp(double value, double min, double max)
+        {
+            return (value < min) ? min : (value > max) ? max : value;
+        }
+    }
+}
